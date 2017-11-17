b8d305e1f6c738e2974874bf8e0ba0ba139553b9
Palayoub <nettimes1@gmail.com>
2017-11-17 19:05:47 +0000

    Adding first Home component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <home></home>
   </div>
 </template>
 
@@ -24,7 +10,7 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Skill Test'
     }
   }
 }
@@ -56,5 +42,11 @@ li {
 
 a {
   color: #42b983;
+}
+.pagination a {
+    color: black;
+    float: left;
+    padding: 8px 16px;
+    text-decoration: none;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div align="center">
+    <input class="inputsearch" type="text" name="search" placeholder="Searching..."/>
+    <button class="button">filter</button>
+    <div >
+      <ul v-for="item in list">
+        - {{ item.text }}
+      </ul>
+      <div class="pagination" v-for="page in pagenum">
+        <a href="#">{{ page }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      list: [
+        { text: 'First repo' },
+        { text: 'Second repo' },
+        { text: 'Third repo' }
+      ],
+      pagenum: 5,
+    }
+  }
+}
+</script>
