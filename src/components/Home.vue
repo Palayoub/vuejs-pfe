073e9bb8916867de21e337439e55076aa70e985a
Palayoub <nettimes1@gmail.com>
2017-11-18 12:39:40 +0000

    Consuming search API in Home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,55 @@
 <template>
   <div>
-    <input class="inputsearch" type="text" name="search" placeholder="Searching..."/>
+    <input class="inputsearch" type="text" name="search" placeholder="Searching..." @input="changedSearch"/>
     <button class="button" @click="filterClicked = !filterClicked">filter</button>
     <filter-search v-if="filterClicked"></filter-search>
     <hr>
-    <div>
-      <ul align="left" v-for="item in list">
-        - {{ item.text }}
+    <span v-if="errors">{{ this.errors.response.data.message }} | Retry in few seconds</span>
+    <div v-else>
+      <ul align="left" v-for="item in repos">
+        - {{ item.full_name }}
       </ul>
-    </div>
-    <div class="pagination" v-for="page in pagenum">
-      <a href="#">{{ page }}</a>
+      <div class="pagination" v-for="page in pagenum">
+        <a href="#" @click="fetchRepos(page)">{{ page }}</a>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+const API = 'https://api.github.com/search/repositories?q=';
 export default {
   data () {
     return {
-      list: [
-        { text: 'First repo' },
-        { text: 'Second repo' },
-        { text: 'Third repo' }
-      ],
+      searchQuery: 'mediax',
       filterClicked: false,
-      pagenum: 5,
+      pagenum: 1,
+      perpage: 20,
+      repos: [],
+      errors: null,
+    }
+  },
+  //fetch result at component created
+  created() {
+    this.fetchRepos(this.pagenum);
+  },
+  methods: {
+  //Method to make a remote request to our API
+    fetchRepos(pagenum) {
+      axios.get(API + this.searchQuery + '&per_page=' + this.perpage + '&page=' + pagenum)
+        .then(response => {
+          //we get the number of pages from the header of our response
+          this.pagenum = parseInt(response.headers.link.split(';')[1].split('=')[4]);
+          this.repos = response.data.items
+        })
+      .catch(e => {
+          this.errors = e;
+        })
+    },
+    //Method to call each time our search input is changed
+    changedSearch(event) {
+      this.searchQuery = event.target.value, this.fetchRepos(this.pagenum);
     }
   }
 }
